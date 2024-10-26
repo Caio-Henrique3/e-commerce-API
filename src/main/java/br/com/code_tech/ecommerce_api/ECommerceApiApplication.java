dd1bb72ef5d29941538f504f055342a3d92cc4ee
Caio-Henrique3 <caioaranha123@hotmail.com>
2024-10-26 00:04:22 -0300

    feat: initial project commit

---
+++ src/main/java/br/com/code_tech/ecommerce_api/ECommerceApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.code_tech.ecommerce_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/code_tech/ecommerce_api/ECommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.code_tech.ecommerce_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
